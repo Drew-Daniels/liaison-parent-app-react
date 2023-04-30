4ab978baa2ec062c2e9bafbfe218dbd77a8a4759
Drew Daniels <daniels_drew@protonmail.com>
2023-04-30 09:41:26 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import { useRef, useEffect } from 'react';
+import { Parent, IParent } from 'post-punk';
+import { nanoid } from 'nanoid';
+
 import './App.css'
-import { sayHi } from 'post-punk';
 
 function App() {
+  const parentRef = useRef<IParent | null>(null);
+
+  useEffect(() => {
+    parentRef.current = Parent({
+      iframeOpts: {
+        id: 'my-embedded-iframe',
+        containerId: 'my-embedded-iframe-container',
+        src: 'http://localhost:3002',
+        classes: ['iframe'],
+      },
+      handlers: {
+        logout: () => console.log('Parent window logging out'),
+        sendToken: () => {
+          const token = nanoid();
+          parentRef.current?.callChildMethod({
+            functionName: 'saveToken',
+            args: token,
+          });
+        }
+      }
+    })
+    parentRef.current.init();
+  }, []);
+
   return (
     <>
+      <h1>Parent Window</h1>
+      <div className='buttons'>
+        <p>Request to run events within the iFrame!</p>
+        <button onClick={initiateChildLogout}>
+          Initiate <em>Child</em> Logout Process
+        </button>
+      </div>
+      <div id="my-embedded-iframe-container" />
     </>
   )
+
+  function initiateChildLogout() {
+    parentRef.current?.callChildMethod({ functionName: 'logout'});
+  }
 }
 
 export default App
